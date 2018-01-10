f3dbe65bfb479097920ce56f850f576c86c51470
dkneis <david.kneis@tu-dresden.de>
2018-01-10 13:56:21 +0000

    ENH: Added data set

---
+++ R/data.r
@@ -0,0 +1,8 @@
+# Documentation of data sets
+
+#' A small set of Escherichia coli growth curves (optical density over time)
+#'
+#' @docType data
+#' @name coli
+#' @author David Kneis \email{david.kneis@@tu-dresden.de}
+NULL
