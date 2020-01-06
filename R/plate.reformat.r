003080e36706bb1d9ce025acd0f76e053a34338f
dkneis <david.kneis@tu-dresden.de>
2020-01-06 09:39:16 +0000

    reformatting of plate reader data

--- R/plate.reformat.r
+++ R/plate.reformat.r
@@ -4,21 +4,18 @@
 #' Specifically, the function merges info from two separate files and also
 #' subtracts the blank value.
 #'
-#' @param file_layout Delimited text file with info on plate layout. Must have
+#' @param layout Data frame with info on plate layout. Must have
 #'   columns 'id_well', 'id_sample', 'id_replicate'. It is necessary that, for
 #'   one or more records, the value in column 'id_sample' is set to 'blank'.
-#' @param file_dynamics Delimited text file with observed data. Must have column
+#' @param dynamics Data frame with observed data. Must have column
 #'   'time'. All additional columns must be named after wells as specified in
 #'   the field 'id_well' of the other input file.
 #'   ".
-#' @param file_out Name for the output file.
-#' @param sep_in The field delimiter for input files. Default is TAB and the
-#'   latter is always used for the output.
-#' @param dec_in The decimal character for input files. Default is period and
-#'   the latter is always used for the output. 
-#' @param overwrite Logical. Should existing output files be overwritten?
 #'   
-#' @return \code{NULL}. All results are written to the specified output file.
+#' @return A data frame.
+#' 
+#' @note The subtracted blank value is the mean of the blank wells for the
+#'   respective time step. Possible negative results are set to zero.
 #'
 #' @author David Kneis \email{david.kneis@@tu-dresden.de}
 #'
@@ -26,43 +23,33 @@
 #'
 #' @examples
 #' \dontrun{
-#'   plate.reformat(file_layout="in/strains.txt", file_dynamics="in/growth.txt",
-#'     file_out="out.txt")
+#'   # Example to be prepared
 #' }
 
-plate.reformat <- function(file_layout, file_dynamics, file_out,
-  sep_in="\t", dec_in=".", overwrite=FALSE) {
+plate.reformat <- function(layout, dynamics) {
 
-  if (!file.exists(file_layout))
-    stop(paste0("input file '",file_layout,"' not found"))
-  plate <- utils::read.table(file=file_layout, header=TRUE, sep=sep_in,
-    stringsAsFactors=FALSE)
   req <- c("id_well","id_sample","id_replicate")
-  if (!all(req %in% names(plate)))
-    stop(paste0("input file '",file_layout,"' must have columns: '",
+  if (!all(req %in% names(layout)))
+    stop(paste0("'layout' must have columns: '",
       paste(req, collapse="', '"),"'"))
-  if (!"blank" %in% plate[,"id_sample"])
-    stop("no record(s) with sample ID 'blank' in file '",file_layout,"'")
-  i <- which(duplicated(plate[,"id_well"]))
+  if (!"blank" %in% layout[,"id_sample"])
+    stop("no record(s) with sample ID 'blank' found in 'layout'")
+  i <- which(duplicated(layout[,"id_well"]))
   if (length(i) > 0)
-    stop("found duplicates well ID(s) in file '",file_layout,
-      "'; first duplicate occurred at line no. ",i[1]+1)
-  i <- which(duplicated(plate[plate$id_sample != "blank",c("id_sample", "id_replicate")]))
+    stop(paste0("found duplicates well ID(s) in 'layout'",
+      "; first duplicate occurred at line no. ",i[1]+1))
+  i <- which(duplicated(layout[layout$id_sample != "blank",c("id_sample", "id_replicate")]))
   if (length(i) > 0)
-    stop("found duplicates in file '",file_layout,
-      "' based on the combined primary key columns 'id_sample' and 'id_replicate'",
-      "; first duplicate occurred at line no. ",i[1]+1)
+    stop(paste0("found duplicates in file 'layout'",
+      " based on the combined primary key columns 'id_sample' and 'id_replicate'",
+      "; first duplicate occurred at line no. ",i[1]+1))
   
-  if (!file.exists(file_dynamics))
-    stop(paste0("input file '",file_dynamics,"' not found"))
-  growth <- utils::read.table(file=file_dynamics, header=TRUE, sep=sep_in, dec=dec_in,
-    check.names=FALSE, stringsAsFactors=FALSE)
   req <- c("time")
-  if (!all(req %in% names(growth)))
-    stop(paste0("input file '",file_dynamics,"' must have columns: '",
+  if (!all(req %in% names(dynamics)))
+    stop(paste0("'dynamics' must have columns: '",
       paste(req, collapse="', '"),"'"))
-  if (length(unique(names(growth))) != length(names(growth)))
-    stop(paste0("column names in input file '",file_dynamics,"' not unique"))
+  if (length(unique(names(dynamics))) != length(names(dynamics)))
+    stop("column names in 'dynamics' not unique")
   
   as.hours <- function(x) {
     x <- unlist(strsplit(x, split=":", fixed=TRUE))
@@ -70,33 +57,28 @@ plate.reformat <- function(file_layout, file_dynamics, file_out,
       stop("bad time string")
     round(sum(as.numeric(x) * c(1, 1/60, 1/3600)), 3)
   }
-  growth$time <- sapply(growth$time, as.hours)
-  if (is.unsorted(growth$time, strictly=TRUE))
-    stop("times not strictly increasing in file '",file_dynamics,"'")
-  
-  growth <- reshape2::melt(data=growth, id.vars="time", variable.name="id_well")
-  bad <- unique(growth$id_well[!growth$id_well %in% plate$id_well])
-  if (length(bad) > 0)
-    stop(paste0("well ID in growth data file '",file_dynamics,
-      "' not listed in plate layout file '",
-      file_layout,"'; Details: '", paste(bad, collapse="', '"),"'"))
-  bad <- plate$id_well[!plate$id_well %in% growth$id_well]
-  if (length(bad) > 0)
-    stop(paste0("well ID in plate layout file '",file_layout,
-      "' not referenced in growth data file '",
-      file_dynamics,"'; Details: '", paste(bad, collapse="', '"),"'"))
-  growth <- merge(x=plate, y=growth, by="id_well")
-  growth$id_well <- NULL
-  rm(plate)
-  
-  blank <- mean(growth$value[growth$id_sample == "blank"])
-  growth$value <- growth$value - blank
-  growth <- growth[growth$id_sample != "blank",]
-  
-  if (file.exists(file_out) && (!overwrite))
-    stop(paste0("unwilling to overwrite existing output file '",file_out,"'"))
-  utils::write.table(growth, file=file_out, sep="\t",
-    col.names=TRUE, row.names=FALSE, quote=FALSE)
+  dynamics$time <- sapply(dynamics$time, as.hours)
+  if (is.unsorted(dynamics$time, strictly=TRUE))
+    stop("times in 'dynamics' not strictly increasing'")
 
-  invisible(NULL)
+  blankWells <- layout$id_well[layout$id_sample == "blank"]
+  blank <- apply(dynamics[,(names(dynamics) %in% blankWells),drop=FALSE], 1, mean)
+  for (i in which(!names(dynamics) %in% c("time", blankWells)))
+    dynamics[,i] <- pmax(0, dynamics[,i] - blank)
+
+  dynamics <- reshape2::melt(data=dynamics, id.vars="time", variable.name="id_well")
+  bad <- unique(dynamics$id_well[!dynamics$id_well %in% layout$id_well])
+  if (length(bad) > 0)
+    stop(paste0("well ID from 'dynamics' not listed in 'layout'",
+      " ; Details: '", paste(bad, collapse="', '"),"'"))
+  bad <- layout$id_well[!layout$id_well %in% dynamics$id_well]
+  if (length(bad) > 0)
+    stop(paste0("well ID from 'layout' not referenced in 'dynamics'",
+      "' ; Details: '", paste(bad, collapse="', '"),"'"))
+  dynamics <- merge(x=layout, y=dynamics, by="id_well")
+  dynamics <- dynamics[dynamics$id_sample != "blank",]
+  dynamics$id_well <- NULL
+
+  return(dynamics)
+  
 }
